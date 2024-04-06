ca936e4198b8187be60f069bb01c41a96bedea2c
DariusC9 <coutidarius@gmail.com>
2024-04-06 15:09:07 +0300

    add service client

--- src/main/java/serviceApp/service/ClientService.java
+++ src/main/java/serviceApp/service/ClientService.java
@@ -1,4 +1,13 @@
 package serviceApp.service;
 
+import serviceApp.domain.Client;
+import serviceApp.repository.Repository;
+
 public class ClientService {
+    private Repository<Client> repository;
+    public ClientService() { }
+    public ClientService(Repository<Client> repository) {
+        this.repository = repository;
+    }
+
 }
