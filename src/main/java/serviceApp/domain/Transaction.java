26f41c61e2495fc8341e012a40b58faf2de01e4b
Ion Iacov <ion.iacov1@gmail.com>
2024-04-06 14:09:23 +0300

    added transaction class

---
+++ src/main/java/serviceApp/domain/Transaction.java
@@ -0,0 +1,84 @@
+package serviceApp.domain;
+
+import java.time.LocalDate;
+
+public class Transaction {
+    private int id;
+    private int id_car;
+    private int id_client;
+    private float partsPrice;
+    private float workPrice;
+    private LocalDate dateHour;
+
+    public Transaction() {
+    }
+
+    public Transaction(int id, int id_car, int id_client, float partsPrice, float workPrice, LocalDate dateHour) {
+        this.id = id;
+        this.id_car = id_car;
+        this.id_client = id_client;
+        this.partsPrice = partsPrice;
+        this.workPrice = workPrice;
+        this.dateHour = dateHour;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId_car() {
+        return id_car;
+    }
+
+    public void setId_car(int id_car) {
+        this.id_car = id_car;
+    }
+
+    public int getId_client() {
+        return id_client;
+    }
+
+    public void setId_client(int id_client) {
+        this.id_client = id_client;
+    }
+
+    public float getPartsPrice() {
+        return partsPrice;
+    }
+
+    public void setPartsPrice(float partsPrice) {
+        this.partsPrice = partsPrice;
+    }
+
+    public float getWorkPrice() {
+        return workPrice;
+    }
+
+    public void setWorkPrice(float workPrice) {
+        this.workPrice = workPrice;
+    }
+
+    public LocalDate getDateHour() {
+        return dateHour;
+    }
+
+    public void setDateHour(LocalDate dateHour) {
+        this.dateHour = dateHour;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "id=" + id +
+                ", id_car=" + id_car +
+                ", id_client=" + id_client +
+                ", partsPrice=" + partsPrice +
+                ", workPrice=" + workPrice +
+                ", dateHour=" + dateHour +
+                '}';
+    }
+}
